a507e6cc63866bcbcca826658874b9f9347fc9d7
srteja123 <39849488+srteja123@users.noreply.github.com>
2024-01-01 18:55:16 +0530

    Update UserRepository.java

--- demo/src/main/java/com/example/demo/repository/UserRepository.java
+++ demo/src/main/java/com/example/demo/repository/UserRepository.java
@@ -3,10 +3,11 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.model.UserInfo;
+import com.example.demo.entity.UserInfo;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserInfo, Long>{
-   Boolean existsByUserEmail(String userEmail);
-     UserInfo findByUserEmail(String userEmail);
+public interface UserRepository extends JpaRepository<UserInfo, Long> {
+  Boolean existsByEmailId(String userEmail);
+
+  UserInfo findByEmailId(String userEmail);
 }

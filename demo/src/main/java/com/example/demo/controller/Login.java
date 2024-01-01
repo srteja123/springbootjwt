fd2a2b3352aeda20be1d7ec226e25cb5e168fa80
srteja123 <39849488+srteja123@users.noreply.github.com>
2024-01-01 18:48:25 +0530

    Update Login.java

--- demo/src/main/java/com/example/demo/controller/Login.java
+++ demo/src/main/java/com/example/demo/controller/Login.java
@@ -1,9 +1,11 @@
 package com.example.demo.controller;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -14,15 +16,17 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.JwtResponse;
 import com.example.demo.dto.LoginRequest;
-import com.example.demo.model.UserInfo;
+import com.example.demo.entity.UserInfo;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.security.jwt.JwtUtils;
 import com.example.demo.service.UserDetailsImpl;
 
+//Email and password are authenticated here and data is being prepared and sent to dashboard
 @RestController
 @RequestMapping("/user")
 @CrossOrigin
@@ -33,7 +37,6 @@ public class Login {
     @Autowired
     UserRepository userRepository;
 
-
     @Autowired
     PasswordEncoder encoder;
 
@@ -41,9 +44,18 @@ public class Login {
     JwtUtils jwtUtils;
 
     @PostMapping("/login")
+    @ResponseBody
     public ResponseEntity<Object> login(@RequestBody LoginRequest request) {
+        HashMap<String, Object> api_response = new HashMap<String, Object>();
         try {
+            Boolean emailExists = userRepository.existsByEmailId(request.getEmailId());
 
+            if(!emailExists){
+                api_response.put("status", "failed");
+                api_response.put("message", "User does not exist.");
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body(api_response);
+
+            }else{
             Authentication authentication = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(request.getEmailId(), request.getPassword()));
 
@@ -51,25 +63,37 @@ public class Login {
             String jwt = jwtUtils.generateJwtToken(authentication);
 
             UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-            List<String> roles = userDetails.getAuthorities().stream()
+             List<String> roles = userDetails.getAuthorities().stream()
                     .map(item -> item.getAuthority())
                     .collect(Collectors.toList());
-                //Login Counter
-                 UserInfo create_record =userRepository.findByUserEmail(userDetails.getEmail());
-                 Long counter = userDetails.getLoginCounter()+ 1L;
-                 create_record.setLoginCounter(counter);
-                 userDetails.setLoginCounter(counter);
-                 userRepository.save(create_record);
-              return ResponseEntity.ok(new JwtResponse(jwt,
-                    userDetails.getId(),
-                    userDetails.getEmail(),
-                    userDetails.getLoginCounter(),
-                    roles)); 
-                    
+            if(userDetails.getAccountStatus().equals("ACTIVE")){
+                UserInfo create_record = userRepository.findByEmailId(userDetails.getEmail());
+                Long counter = userDetails.getLoginCounter() + 1L;
+                create_record.setLoginCounter(counter);
+                userDetails.setLoginCounter(counter);
+                userRepository.save(create_record);
+                return ResponseEntity.ok(new JwtResponse(jwt,
+                        userDetails.getId(),
+                        userDetails.getEmail(),
+                        userDetails.getLoginCounter(),
+                        roles));
+            }else{
+                api_response.put("status", "failed");
+                api_response.put("message", "User account is de-activated");
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body(api_response);
+            }
+        }
         } catch (Exception e) {
-            // TODO: handle exception
-            
-            return ResponseEntity.ok("CATCH OUT " + e.getMessage());
+            if (e.getMessage().equalsIgnoreCase("Bad credentials")) {
+                api_response.put("status", "failed");
+                api_response.put("message", e.getMessage());
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body(api_response);
+            } else {
+                api_response.put("status", "failed");
+                api_response.put("message", e.getMessage());
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(api_response);
+            }
+
         }
     }
 }

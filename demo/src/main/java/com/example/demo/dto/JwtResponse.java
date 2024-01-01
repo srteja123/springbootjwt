94d148da13c2efc1e678fc6296a3ac061c68eeaf
srteja123 <39849488+srteja123@users.noreply.github.com>
2024-01-01 18:51:41 +0530

    Update JwtResponse.java

--- demo/src/main/java/com/example/demo/dto/JwtResponse.java
+++ demo/src/main/java/com/example/demo/dto/JwtResponse.java
@@ -9,58 +9,52 @@ public class JwtResponse {
   private String email;
   private List<String> roles;
   private Long loginCounter;
-
-  public JwtResponse(String accessToken, Long id, String email,Long loginCounter,  List<String> roles) {
-    this.token = accessToken;
-    this.id = id;
-    this.email = email;
-    this.loginCounter = loginCounter;
-    this.roles = roles;
-  }
-
-  public String getAccessToken() {
+  public String getToken() {
     return token;
   }
-
-  public void setAccessToken(String accessToken) {
-    this.token = accessToken;
+  public void setToken(String token) {
+    this.token = token;
   }
-
-  public String getTokenType() {
+  public String getType() {
     return type;
   }
-
-  public void setTokenType(String tokenType) {
-    this.type = tokenType;
+  public void setType(String type) {
+    this.type = type;
   }
-
   public Long getId() {
     return id;
   }
-
   public void setId(Long id) {
     this.id = id;
   }
-
   public String getEmail() {
     return email;
   }
-
   public void setEmail(String email) {
     this.email = email;
   }
-
-
   public List<String> getRoles() {
     return roles;
   }
-
+  public void setRoles(List<String> roles) {
+    this.roles = roles;
+  }
   public Long getLoginCounter() {
     return loginCounter;
   }
-
   public void setLoginCounter(Long loginCounter) {
     this.loginCounter = loginCounter;
   }
+  
+  
+  public JwtResponse(String token, Long id, String email,  Long loginCounter,List<String> roles) {
+    this.token = token;
+    this.id = id;
+    this.email = email;
+    this.roles = roles;
+    this.loginCounter = loginCounter;
+  }
+
+  
   
 }

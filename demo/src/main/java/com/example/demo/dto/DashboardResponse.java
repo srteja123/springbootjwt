3e1e9bcde333324d9ce8b4380625e52cf57deda6
srteja123 <39849488+srteja123@users.noreply.github.com>
2024-01-01 18:51:11 +0530

    Update DashboardResponse.java

--- demo/src/main/java/com/example/demo/dto/DashboardResponse.java
+++ demo/src/main/java/com/example/demo/dto/DashboardResponse.java
@@ -2,49 +2,39 @@ package com.example.demo.dto;
 
 public class DashboardResponse {
     private String email;
-  private String userType;
-  private Long loginCounter;
-  private Long id;
-  
+    private String userType;
+    private Long loginCounter;
+    private Long id;
+    
+    public DashboardResponse(String email, String userType, Long loginCounter, Long id) {
+        this.email = email;
+        this.userType = userType;
+        this.loginCounter = loginCounter;
+        this.id = id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getUserType() {
+        return userType;
+    }
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+    public Long getLoginCounter() {
+        return loginCounter;
+    }
+    public void setLoginCounter(Long loginCounter) {
+        this.loginCounter = loginCounter;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-public DashboardResponse(String email, String userType, Long loginCounter, Long id) {
-    this.email = email;
-    this.userType = userType;
-    this.loginCounter = loginCounter;
-    this.id = id;
-}
-
-public String getEmail() {
-    return email;
-}
-
-public void setEmail(String email) {
-    this.email = email;
-}
-
-public Long getLoginCounter() {
-    return loginCounter;
-}
-
-public void setLoginCounter(Long loginCounter) {
-    this.loginCounter = loginCounter;
-}
-
-public Long getId() {
-    return id;
-}
-
-public void setId(Long id) {
-    this.id = id;
-}
-
-public String getUserType() {
-    return userType;
-}
-
-public void setUserType(String userType) {
-    this.userType = userType;
-}
-
-  
 }

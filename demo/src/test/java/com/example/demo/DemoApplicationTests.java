2cdfbbf3d1873f58bcedbb231490193f77f95fb2
srteja123 <39849488+srteja123@users.noreply.github.com>
2024-01-01 18:42:14 +0530

    Update DemoApplicationTests.java

---
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.*;
+
+public class DemoApplicationTest {
+	@Test
+	public void main() {
+		String[] args = { "abc", "abc", "abc" };
+		DemoApplication.main(args);
+	}
+}
